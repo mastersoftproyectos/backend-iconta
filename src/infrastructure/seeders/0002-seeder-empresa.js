2f4d54b7794cceb9c388e444457c0643b3fef886
Ivan Tancara <vantcfanel123@gmail.com>
2024-06-04 06:44:38 -0400

    Empresa

--- src/infrastructure/seeders/0002-seeder-empresa.js
+++ src/infrastructure/seeders/0002-seeder-empresa.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const { CONFIGURACION_EMPRESA_DEFECTO } = require('../../common/config/constants');
 const { setTimestampsSeeder } = require('../lib/util');
 
 let items = [
-  { id: 'ee554d08-6dd4-4b2d-882c-3791d9dbef96', nit: '924864302',  nombre_comercial: 'MasterSoft', correo_electronico: 'mastersoft.proyectos@gmail.com', estado: 'ACTIVO' },
-  { id: 'ee5d3f27-107e-4863-9d07-6a086c8ed11a', nit: '9248643012',  nombre_comercial: 'SAMEQ', correo_electronico: 'sameq@gmail.com', estado: 'ACTIVO' }
+  { id: 'ee554d08-6dd4-4b2d-882c-3791d9dbef96', nit: '924864302',  nombre_comercial: 'MasterSoft', configuracion: JSON.stringify(CONFIGURACION_EMPRESA_DEFECTO), correo_electronico: 'mastersoft.proyectos@gmail.com', estado: 'ACTIVO' },
+  { id: 'ee5d3f27-107e-4863-9d07-6a086c8ed11a', nit: '9248643012',  nombre_comercial: 'SAMEQ', configuracion: JSON.stringify(CONFIGURACION_EMPRESA_DEFECTO), correo_electronico: 'sameq@gmail.com', estado: 'ACTIVO' }
 ];
 
 // Asignando datos de log y timestamps a los datos
